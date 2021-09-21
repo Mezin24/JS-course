531989aaac24c11a6414b3aba787b307cf385e09
Pavel Mezencev <pamezencev@gmail.com>
2021-09-21 22:47:23 +0300

    finished 'Guess my number game'

--- games/Guess my number/script.js
+++ games/Guess my number/script.js
@@ -8,54 +8,61 @@ const labelMessage = document.querySelector('.message');
 const labelScore = document.querySelector('.score');
 const labelHighscore = document.querySelector('.highscore');
 
-// create a random number between 1 and 20
 function createRandom() {
-    return Math.floor(Math.random() * 20) + 1;
+  return Math.floor(Math.random() * 20) + 1;
 }
+
 function startGame() {
-    randomNumber = createRandom();
-    currentScore = 5;
-    console.log(randomNumber)
-    labelMessage.textContent = 'Start guessing...';
-    labelScore.textContent = currentScore
-    labelNumber.textContent = '?'
-    document.body.style.backgroundColor = '#222'
-    input.value = '';
-    isOver = false
+  randomNumber = createRandom();
+  currentScore = 10;
+  console.log(randomNumber);
+  labelMessage.textContent = 'Start guessing...';
+  labelScore.textContent = currentScore;
+  labelNumber.textContent = '?';
+  document.body.style.backgroundColor = '#222';
+  input.value = '';
+  isOver = false;
 }
+function wrongAnswer(num) {
+  labelMessage.textContent =
+    num > randomNumber ? '🔝 too much' : '🔙 too little';
+  labelScore.textContent = --currentScore;
+}
+
 let randomNumber, currentScore;
-let isOver = false
+let highscore = 0;
+let isOver = false;
 startGame();
 
 btnCheck.addEventListener('click', () => {
-    let currentNumber = +input.value;
+  let currentNumber = +input.value;
 
-    if (!currentNumber || isOver) return
-    
-    if (currentNumber > randomNumber) {
-        labelMessage.textContent = '🔝 too much'
-        labelScore.textContent = --currentScore;
-    } else if (currentNumber < randomNumber) {
-        labelMessage.textContent = '🔙 too little'
-        labelScore.textContent = --currentScore;
-        
-    } else {
-        labelMessage.textContent = '🔥 CORRECT'
-        document.body.style.backgroundColor = '#60b347'
-        labelNumber.textContent = randomNumber
-        let highScore = +labelHighscore.textContent
-        isOver = true
+  if (!currentNumber) labelMessage.textContent = '⛔ No mumber!';
 
-        if (currentScore > highScore) labelHighscore.textContent = currentScore
+  if (!currentNumber || isOver) return;
+
+  if (currentNumber > randomNumber) {
+    wrongAnswer(currentNumber);
+  } else if (currentNumber < randomNumber) {
+    wrongAnswer(currentNumber);
+  } else {
+    labelMessage.textContent = '🔥 CORRECT';
+    document.body.style.backgroundColor = '#60b347';
+    labelNumber.textContent = randomNumber;
+    isOver = true;
+
+    if (currentScore > highscore) {
+      highscore = currentScore;
+      labelHighscore.textContent = highscore;
     }
-    input.value = '';
+  }
+  input.value = '';
 
-    if (currentScore === 0) {
-        labelMessage.textContent = 'The game os over!'
-        isOver = true
-    }
-    
-})  
+  if (currentScore === 0) {
+    labelMessage.textContent = 'The game is over!';
+    isOver = true;
+  }
+});
 btnAgain.addEventListener('click', () => {
-    startGame();
-})
+  startGame();
+});
