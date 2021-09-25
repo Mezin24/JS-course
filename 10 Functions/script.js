bcfcc177461f7b4fd82f00629e2d7de9b658f0dd
Pavel Mezencev <mezencev24@mail.ru>
2021-09-25 11:18:53 +0300

    finished 10 section

--- 10 Functions/script.js
+++ 10 Functions/script.js
@@ -160,16 +160,51 @@ poll.displayResults.call([1, 5, 3, 9, 6, 1]);
 poll.displayResults.call([5, 2, 3], 'string');
 poll.displayResults.call([1, 5, 3, 9, 6, 1], 'string');
 */
-let a = 1;
-//CLOSERS
-const counter = function () {
-  let count = 0;
-  return function () {
-    return ++count;
-  };
-};
-const counter1 = counter();
-const counter2 = counter();
+// let a = 1;
+// //CLOSERS
+// const counter = function () {
+//   let count = 0;
+//   return function () {
+//     return ++count;
+//   };
+// };
+// const counter1 = counter();
+// const counter2 = counter();
 
-console.dir(counter);
-console.dir(counter1);
+// console.dir(counter);
+// console.dir(counter1);
+
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// g();
+// f();
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function f1() {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', () => {
+    // header.style.color = 'blue';
+    header.classList.toggle('blue');
+  });
+})();
