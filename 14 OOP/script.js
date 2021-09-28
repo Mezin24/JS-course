d9c3f4f7705ac2a5f0f4f69db90d97e4eaa303ec
Pavel Mezencev <mezencev24@mail.ru>
2021-09-28 17:35:25 +0300

    start working with OOP section

---
+++ 14 OOP/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// function User(name, birthyear) {
+//   this.name = name;
+//   this.birthyear = birthyear;
+// }
+// User.prototype.sayHi = function () {
+//   console.log(`Hello ${this.name}`);
+// };
+// User.prototype.calcAge = function () {
+//   console.log(this);
+//   this.age = new Date().getFullYear() - this.birthyear;
+//   console.log(`My name is ${this.name} and I am ${this.age} years old!`);
+// };
+
+// class User {
+//   constructor(name, birthYear) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//   }
+//   sayHi() {
+//     console.log(`Hello ${this.name}`);
+//   }
+//   calcAge() {
+//     console.log(this);
+//     this.age = new Date().getFullYear() - this.birthYear;
+//     console.log(`My name is ${this.name} and I am ${this.age} years old!`);
+//   }
+// }
+
+// const pavel = new User('Pavel', 1987);
+// console.log(pavel);
+// pavel.sayHi();
+// pavel.calcAge();
+// console.log(pavel);
